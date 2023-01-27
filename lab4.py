2576099fb8e54d7d1b0be6ef7f64381a6c5bc371
Aruzhan152 <aruzhanshamshiyeva@gmail.com>
2023-01-27 13:25:57 +0600

    4th lab

---
+++ lab4.py
@@ -0,0 +1,20 @@
+x=5
+print(int(x))
+
+x="Hello, World"
+print(str(x))
+
+x = 20.5
+print(float(x))
+
+x = ["apple", "banana", "cherry"]
+print(list(x))
+
+x = ("apple", "banana", "cherry")
+print(tuple(x))
+
+x = {"name" : "John", "age" : 36}
+print(dict(x))
+
+x = True
+print(bool(x))
