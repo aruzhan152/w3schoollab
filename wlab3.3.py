1c8c4fc4fca1ad5304058948c1ef16ad2ee75f10
Aruzhan152 <aruzhanshamshiyeva@gmail.com>
2023-02-08 23:02:55 +0600

    functions 2

---
+++ wlab3.3.py
@@ -0,0 +1,107 @@
+#1
+from movies import movies
+def good(movie):
+    return movie["imdb"] > 5.5
+
+def sublist(movies):
+    subl = list()
+    for movie in movies:
+        if(good(movie)):
+            subl.append(movie)
+    return subl
+
+def filterMovies(movies, category):
+    subl = list()
+    for movie in movies:
+        if(movie["category"] == category):
+            subl.append(movie)
+    return subl
+
+def avgScore(movies):
+    sum = 0
+    for movie in movies:
+        sum += movie["imdb"]
+    return sum/len(movies)
+
+def avgCategoryScore(movies, category):
+    subl = filterMovies(movies, category)
+    return avgScore(subl)
+
+#2
+movies = [
+{
+"name": "Usual Suspects", 
+"imdb": 7.0,
+"category": "Thriller"
+},
+{
+"name": "Hitman",
+"imdb": 6.3,
+"category": "Action"
+},
+{
+"name": "Dark Knight",
+"imdb": 9.0,
+"category": "Adventure"
+},
+{
+"name": "The Help",
+"imdb": 8.0,
+"category": "Drama"
+},
+{
+"name": "The Choice",
+"imdb": 6.2,
+"category": "Romance"
+},
+{
+"name": "Colonia",
+"imdb": 7.4,
+"category": "Romance"
+},
+{
+"name": "Love",
+"imdb": 6.0,
+"category": "Romance"
+},
+{
+"name": "Bride Wars",
+"imdb": 5.4,
+"category": "Romance"
+},
+{
+"name": "AlphaJet",
+"imdb": 3.2,
+"category": "War"
+},
+{
+"name": "Ringing Crime",
+"imdb": 4.0,
+"category": "Crime"
+},
+{
+"name": "Joking muck",
+"imdb": 7.2,
+"category": "Comedy"
+},
+{
+"name": "What is the name",
+"imdb": 9.2,
+"category": "Suspense"
+},
+{
+"name": "Detective",
+"imdb": 7.0,
+"category": "Suspense"
+},
+{
+"name": "Exam",
+"imdb": 4.2,
+"category": "Thriller"
+},
+{
+"name": "We Two",
+"imdb": 7.2,
+"category": "Romance"
+}
+]
